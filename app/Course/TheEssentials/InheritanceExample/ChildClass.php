3c2a2ad95d9d6df368c773df83a6c3d2d45784ae
Mohamed Khalaf <mohamedkhalaf0912@gmail.com>
2020-06-02 09:58:47 +0200

    Inheritance  Example

---
+++ app/Course/TheEssentials/InheritanceExample/ChildClass.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Course\TheEssentials\InheritanceExample;
+
+
+class ChildClass extends ParentClass
+{
+
+}
---
+++ app/Course/TheEssentials/InheritanceExample/ParentClass.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Course\TheEssentials\InheritanceExample;
+
+
+class ParentClass
+{
+    protected $a;
+    public function sayA()
+    {
+        echo "A <br/>";
+    }
+    private function sayB()
+    {
+        echo "B <br/>";
+    }
+    public  function sayC()
+    {
+        echo "C <br/>";
+    }
+
+
+
+
+}
